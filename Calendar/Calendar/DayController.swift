9d661e2b50ca636e25e0872e6301ca72e18fd937
huangmubin <huangmubin@qq.com>
2016-11-15 18:09:25 +0800

    -Calendar update

---
+++ Calendar/Calendar/DayController.swift
@@ -0,0 +1,21 @@
+//
+//  DayController.swift
+//  Calendar
+//
+//  Created by 黄穆斌 on 2016/11/15.
+//  Copyright © 2016年 Myron. All rights reserved.
+//
+
+import UIKit
+
+class DayController: UIViewController {
+
+    
+    // MARK: - Day View
+    
+    @IBOutlet weak var dayView: DayView!
+    @IBOutlet weak var dayViewPre: DayView!
+    @IBOutlet weak var dayViewNext: DayView!
+    
+    
+}
--- Calendar/Calendar/DayView.swift
+++ Calendar/Calendar/DayView.swift
@@ -10,12 +10,9 @@ import UIKit
 
 class DayView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var chineseLabel: UILabel!
+    
 }
