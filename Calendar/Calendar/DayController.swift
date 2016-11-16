79b0859cc249ecf1951e7845afa0e10b25dc5755
huangmubin <hmb87381041@163.com>
2016-11-16 08:20:35 +0800

    -Calendar update

--- Calendar/Calendar/DayController.swift
+++ Calendar/Calendar/DayController.swift
@@ -17,5 +17,52 @@ class DayController: UIViewController {
     @IBOutlet weak var dayViewPre: DayView!
     @IBOutlet weak var dayViewNext: DayView!
     
+    @IBOutlet weak var dayViewLayout: NSLayoutConstraint!
+    
+    @IBAction func dayViewPanAction(_ sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .began:
+            break
+        case .changed:
+            dayViewLayout.constant = sender.translation(in: view).x
+        case .ended:
+            
+            // Pre
+            
+            if dayViewLayout.constant < -view.bounds.width / 2 {
+                UIView.animate(withDuration: 0.2, animations: {
+                    self.dayViewLayout.constant = -self.view.bounds.width
+                    self.view.layoutIfNeeded()
+                    }, completion: { (finish) in
+                        self.dayViewLayout.constant = 0
+                        self.view.layoutIfNeeded()
+                })
+                return
+            }
+            
+            // Next
+            
+            if dayViewLayout.constant > view.bounds.width / 2 {
+                UIView.animate(withDuration: 0.2, animations: {
+                    self.dayViewLayout.constant = self.view.bounds.width
+                    self.view.layoutIfNeeded()
+                    }, completion: { (finish) in
+                        self.dayViewLayout.constant = 0
+                        self.view.layoutIfNeeded()
+                })
+                return
+            }
+            
+            // Center
+            fallthrough
+        default:
+            UIView.animate(withDuration: 0.2) {
+                self.dayViewLayout.constant = 0
+                self.view.layoutIfNeeded()
+            }
+        }
+        
+    }
+    
     
 }
